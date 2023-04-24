8c28da4af3182e42b6e7cc1ce922a2388de27c95
sert121 <yashrajmore29@gmail.com>
2023-04-25 00:52:15 +0530

    integration url

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,8 @@ import {useSearchParams} from 'react-router-dom';
 
 
 function HomePage() {
-    let REACT_APP_NOTION_AUTH_URL= "https://api.notion.com/v1/oauth/authorize?client_id=058b35d0-184c-40ee-9d72-0e38e8a26632&response_type=code&owner=user&redirect_uri=http%3A%2F%2Flocalhost%3A3000"
-    
+    // let REACT_APP_NOTION_AUTH_URL= "https://api.notion.com/v1/oauth/authorize?client_id=058b35d0-184c-40ee-9d72-0e38e8a26632&response_type=code&owner=user&redirect_uri=http%3A%2F%2Flocalhost%3A3000"
+    let REACT_APP_NOTION_AUTH_URL = process.env.REACT_APP_NOTION_AUTH_URL;
     let [searchParams, setSearchParams] = useSearchParams()
     
     useEffect(() => {
